882a04dad397cf4c7b4e83362d9fb3c8ae429590
erichjacobs <erichjacobs@users.noreply.github.com>
2020-05-12 08:11:43 -0300

    bank changes

---
+++ Código/Transfers.c
@@ -0,0 +1,19 @@
+void actions()
+{
+    int transferType;
+    printf("  ------------------------------------------------------------\n");
+    printf("  |                    ... FATEC BANK ...                    |\n");
+    printf("  |                    ..................                    |\n");
+    printf("  |                      TRANSFERENCIAS                      |\n");
+    printf("  |                                                          |\n");
+    printf("  |     <- CONTA CORRERNTE (1)      (2) CONTA POLPANCA ->    |\n");
+    printf("  |                                                          |\n");
+    printf("  |     <- VOLTAR (3)                                        |\n");
+    printf("  |                                                          |\n");
+    printf("  |                      DIGITE A OPCAO                      |\n");
+    printf("  |                                                          |\n");
+    printf("  |                                                          |\n");
+    printf("  |                                                          |\n");
+    printf("  ------------------------------------------------------------\n");
+    scanf("%d"< transferType);
+}
---
+++ Código/saques.c
@@ -0,0 +1,13 @@
+void actions()
+{
+    int amount;
+    printf("  ------------------------------------------------------------\n");
+    printf("  |                    ... FATEC BANK ...                    |\n");
+    printf("  |                    ..................                    |\n");
+    printf("  |                         SAQUES                           |\n");
+    printf("  |                                                          |\n");
+    printf("  |               DIGITE O VALOR A SER SACADO                |\n");
+    printf("  |                                                          |\n");
+    printf("  ------------------------------------------------------------\n");
+    scanf("%d", amount);
+}
